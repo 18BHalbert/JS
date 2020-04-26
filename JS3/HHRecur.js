66cc3425092d5d2e8f70dc8b28980fde43b6f10d
Benjamin Halbert <bhalbert2@mail.greenriver.edu>
2020-04-26 02:02:48 -0400

    made edits to the submission

--- JS3/HHRecur.js
+++ JS3/HHRecur.js
@@ -1,4 +1,17 @@
-function HeeHawRecur($input){
+document.getElementById("HHform").onsubmit = Validate;
+
+function Validate(){
+    let num = document.getElementById("input").value;
+    if(typeof num == 'number'){
+        HeeHawRecur(num);
+    }
+    else{
+        document.write("please enter a number");
+        document.write(typeof num);
+    }
+}
+
+function HeeHawRecur(num){
     if($input == 0){
         return;
     }
